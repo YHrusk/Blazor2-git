42e59643b6967fbce6d95ab4da8001f141004440
YHrusk <hruskovicova.i@gmail.com>
2022-03-04 17:52:24 +0100

    pridani slozek hadac-cisel a nemocnice

---
+++ Nemocnice/PptNemocnice/vybaveniModel.cs
@@ -0,0 +1,18 @@
+﻿namespace PptNemocnice
+{
+    public class vybaveniModel
+    {
+        public string Name { get; set; }
+        public DateTime BoughtDate { get; set; }
+        public DateTime LastRevisionDate { get; set; }
+        public bool NeedRevision { get;}
+
+        public vybaveniModel(string nazev, DateTime zakoupeno, DateTime revize, bool potrebaRevize)
+        {
+            Name = nazev;
+            BoughtDate = zakoupeno;
+            LastRevisionDate = revize;
+            NeedRevision = potrebaRevize;
+        }
+    }
+}
