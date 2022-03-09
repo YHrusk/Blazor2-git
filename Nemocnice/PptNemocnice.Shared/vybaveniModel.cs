4b3dc02578292f7e8e9fd75bee55acf7e6d36e3a
YHrusk <hruskovicova.i@gmail.com>
2022-03-09 20:31:27 +0100

    zmena nemocnice

---
+++ Nemocnice/PptNemocnice.Shared/vybaveniModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PptNemocnice.Shared
+{
+    public class vybaveniModel
+    {
+        public string Name { get; set; }
+        public DateTime BoughtDate { get; set; }
+        public DateTime LastRevisionDate { get; set; }
+        public bool NeedRevision => DateTime.Now - LastRevisionDate > TimeSpan.FromDays(365 * 2);
+
+        public bool IsInEditMode { get; set; }
+
+        public vybaveniModel(string nazev, DateTime zakoupeno, DateTime revize, bool uprava)
+        {
+            Name = nazev;
+            BoughtDate = zakoupeno;
+            LastRevisionDate = revize;
+            IsInEditMode = uprava;
+        }
+    }
+}
