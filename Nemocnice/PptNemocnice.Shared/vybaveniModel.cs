e33a7f8fa7d121b3c07f9a681cef9a78c7489253
YHrusk <hruskovicova.i@gmail.com>
2022-03-17 15:32:16 +0100

    pridani zaznamu, childcontent, validace, cena

--- Nemocnice/PptNemocnice.Shared/vybaveniModel.cs
+++ Nemocnice/PptNemocnice.Shared/vybaveniModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,19 +9,31 @@ namespace PptNemocnice.Shared
 {
     public class VybaveniModel
     {
+        [Required, MinLength(5, ErrorMessage = "Délka u pole \"{0}\" musí být alespoň {1} znaků")]
+        [Display(Name = "Název")]
         public string Name { get; set; }
         public DateTime BoughtDate { get; set; }
         public DateTime LastRevisionDate { get; set; }
         public bool NeedRevision => DateTime.Now - LastRevisionDate > TimeSpan.FromDays(365 * 2);
-
         public bool IsInEditMode { get; set; }
 
-        public VybaveniModel(string nazev, DateTime zakoupeno, DateTime revize, bool uprava)
+        [Range(1, 10000000, ErrorMessage = "Cena musí být mezi 1 a 10 000 000.")]
+        //[Display(Name = "Cena")]
+        public int Price { get; set; }
+
+        public VybaveniModel(string nazev, DateTime zakoupeno, DateTime revize, bool uprava, int cena)
         {
             Name = nazev;
             BoughtDate = zakoupeno;
             LastRevisionDate = revize;
             IsInEditMode = uprava;
+            Price = cena;
         }
+
+        public VybaveniModel()              //prazdny konstruktor pro vytvoreni newEntity
+        {
+                
+        }
+
     }
 }
