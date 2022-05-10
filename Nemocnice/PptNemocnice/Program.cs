12a02059f43517d37beb40ec9835a676a1bc728a
YHrusk <hruskovicova.i@gmail.com>
2022-05-10 17:37:56 +0200

    13-oprava

--- Nemocnice/PptNemocnice/Program.cs
+++ Nemocnice/PptNemocnice/Program.cs
@@ -8,7 +8,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 var apiUri = builder.Configuration["ApiUri"];
 if (apiUri == null) throw new ArgumentNullException(nameof(apiUri));
-
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiUri) });
 
 await builder.Build().RunAsync();
