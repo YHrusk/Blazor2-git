214b830003ff53f7b87111ed4f685cad09cd5e2c
YHrusk <hruskovicova.i@gmail.com>
2022-05-10 17:30:56 +0200

    13-appsettings

--- Nemocnice/PptNemocnice.api/Program.cs
+++ Nemocnice/PptNemocnice.api/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddCors(corsOptions => corsOptions.AddDefaultPolicy(policy =>
-    policy.WithOrigins("https://localhost:7132")
+    policy.WithOrigins(builder.Configuration["AllowedOrigins"])
     .WithMethods("GET", "POST", "DELETE", "PUT")
     .AllowAnyHeader()
 ));
--- Nemocnice/PptNemocnice/Program.cs
+++ Nemocnice/PptNemocnice/Program.cs
@@ -3,9 +3,12 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using PptNemocnice;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+var apiUri = builder.Configuration["ApiUri"];
+if (apiUri == null) throw new ArgumentNullException(nameof(apiUri));
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7167") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiUri) });
 
 await builder.Build().RunAsync();
